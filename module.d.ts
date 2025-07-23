88b7328ecc1d402a5613f81bed6c1d072fcd87ac
Craig Riley <68274157+craigrileyuk@users.noreply.github.com>
2025-07-23 02:14:26 +0100

    Update Vue and Inertia types

--- module.d.ts
+++ module.d.ts
@@ -1,5 +1,5 @@
 // module.d.ts
-
+export {}
 declare module "adminui" {
   import * as Vue from "vue";
 
@@ -14,28 +14,53 @@ declare module "adminui" {
     reload: Array<string>;
   }
 
-  export function ref<T extends Vue.Ref>(value: T): T;
-  export function ref<T>(value: T): Vue.Ref<Vue.UnwrapRef<T>>;
-  export function ref<T = any>(): Vue.Ref<T | undefined>;
+  const ref: typeof import("vue")["ref"]
+  const computed: typeof import("vue")["computed"]
+  const reactive: typeof import("vue")["reactive"]
+  const inject: typeof import("vue")["inject"]
+  const provide: typeof import("vue")["provide"]
+  const getCurrentInstance: typeof import("vue")["getCurrentInstance"]
+  const watch: typeof import("vue")["watch"]
+  const watchEffect: typeof import("vue")["watchEffect"]
+  const nextTick: typeof import("vue")["nextTick"]
+  const onBeforeMount: typeof import("vue")["onBeforeMount"]
+  const onMounted: typeof import("vue")["onMounted"]
+  const onBeforeUnmount: typeof import("vue")["onBeforeUnmount"]
+  const onUnmounted: typeof import("vue")["onUnmounted"]
+  const onUpdated: typeof import("vue")["onUpdated"]
+  const onActivated: typeof import("vue")["onActivated"]
+  const onDeactivated: typeof import("vue")["onDeactivated"]
+  const onErrorCaptured: typeof import("vue")["onErrorCaptured"]
+  const readonly: typeof import("vue")["readonly"]
+  const toRaw: typeof import("vue")["toRaw"]
+  const markRaw: typeof import("vue")["markRaw"]
+  const isRef: typeof import("vue")["isRef"]
+  const isProxy: typeof import("vue")["isProxy"]
+  const unref: typeof import("vue")["unref"]
+  const toRef: typeof import("vue")["toRef"]
+  const toRefs: typeof import("vue")["toRefs"]
+  const isReactive: typeof import("vue")["isReactive"]
+  const isReadonly: typeof import("vue")["isReadonly"]
+  const shallowRef: typeof import("vue")["shallowRef"]
+  const h: typeof import("vue")["h"]
+  const useTemplateRef: typeof import("vue")["useTemplateRef"]
+  const useId: typeof import("vue")["useId"]
+  const toValue: typeof import("vue")["toValue"]
+  const useSlots: typeof import("vue")["useSlots"]
+  const useAttrs: typeof import("vue")["useAttrs"]
+  const defineAsyncComponent: typeof import("vue")["defineAsyncComponent"]
+  const useModel: typeof import("vue")["useModel"]
+  const hydrateOnIdle: typeof import("vue")["hydrateOnIdle"]
+  const hydrateOnVisible: typeof import("vue")["hydrateOnVisible"]
+  const hydrateOnMediaQuery: typeof import("vue")["hydrateOnMediaQuery"]
+  const hydrateOnInteraction: typeof import("vue")["hydrateOnInteraction"]
 
-  export const watch: (
-    expOrFn: string | (() => any),
-    cb: Function,
-    options?: Record<string, any>
-  ) => Function;
-
-  export function reactive<T extends object>(
-    target: T
-  ): Vue.UnwrapNestedRefs<T>;
-
-  export function computed<T>(
-    getter: Vue.ComputedGetter<T>,
-    debugOptions?: Vue.DebuggerOptions
-  ): Vue.ComputedRef<T>;
-  export function computed<T>(
-    options: Vue.WritableComputedOptions<T>,
-    debugOptions?: Vue.DebuggerOptions
-  ): Vue.WritableComputedRef<T>;
+  const usePage: import("@inertiajs/vue3")["usePage"]
+  const useForm: import("@inertiajs/vue3")["useForm"]
+  const router: import("@inertiajs/vue3")["router"]
+  const useRoute: import("@inertiajs/vue3")["useRoute"]
+  const Link: import("@inertiajs/vue3")["Link"]
+  const Head: import("@inertiajs/vue3")["Head"]
 
   interface ApiForm {
     form: object;
